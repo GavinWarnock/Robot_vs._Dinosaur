55fc2d21ce95d8c877bdcb6800889ba700573579
Gavin Warnock <gbwarnock@gmail.com>
2023-02-17 10:32:25 -0700

    Created working Robot attavk method

--- robot.py
+++ robot.py
@@ -6,7 +6,7 @@ class Robot:
         self.health = 100
         self.active_weapon = Weapon
     def attack(self, dinosaur):
-        dinosaur.health -= active_weapon
+        dinosaur.health -= active_weapon.attack_power
         print(dinosaur.health)
 
 active_weapon = Weapon("Blaster Cannon", 60)
