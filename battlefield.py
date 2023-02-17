53c30eb101a4a95d383c5ac45cbb054f73bb7162
Gavin Warnock <gbwarnock@gmail.com>
2023-02-17 14:02:37 -0700

    Project done beginning working on bonus section

--- battlefield.py
+++ battlefield.py
@@ -1,44 +1,45 @@
 from robot import Robot
 from dinosaur import Dinosaur
-Sauron = Dinosaur("Sauron the Great", 20)
-Megatron = Robot("Megatron")
+
 
 class Battlefield:
     def __init__(self,name):
+        self.dinosaur_one = Dinosaur("Sauron the Great", 20)
+        self.robot_one = Robot("Megatron")
         self.name = name
     def display_welcome(self):
-            print(f"Welcome to the {self.name}! Where the futuristic {Megatron.name} will face off against the primal fury of {Sauron.name}!")
+            print(f"Welcome to the {self.name}! Where the futuristic {self.robot_one.name} will face off against the primal fury of {self.dinosaur_one.name}!")
             print("")
     def display_winner(self):
-        if Megatron.health <= 0:
-            print(f"{Sauron.name} is victorious!")
-        elif Sauron.health <= 0:
-            print(f"{Megatron.name} is victorious!")
+        if self.robot_one.health <= 0:
+            print(f"{self.dinosaur_one.name} is victorious!")
+        elif self.dinosaur_one.health <= 0:
+            print(f"{self.robot_one.name} is victorious!")
     def battle_phase(self):     
         start = input("Are you ready to begin? Yes or No ")
         if start == "Yes" or start == "yes":
-            while Megatron.health >= 0 or Sauron.health >= 0:
-                Megatron.attack(Sauron)
-                if Megatron.health <= 0:
-                    print(f"{Megatron.name} has fallen")
-                    winner = Sauron.name
-                    return winner
+            while self.robot_one.health >= 0 or self.dinosaur_one.health >= 0:
+                self.robot_one.attack(self.dinosaur_one)
+                if self.robot_one.health <= 0:
+                    print(f"{self.robot_one.name} has fallen")
+                    winner = self.dinosaur_one.name
+                    break
                     
-                elif Sauron.health <= 0:
-                    print(f"{Sauron.name} has fallen")
-                    winner = Megatron.name
-                    return winner
+                elif self.dinosaur_one.health <= 0:
+                    print(f"{self.dinosaur_one.name} has fallen")
+                    winner = self.robot_one.name
+                    break
                     
-                Sauron.attack(Megatron)
-                if Megatron.health <= 0:
-                    print(f"{Megatron.name} has fallen")
-                    winner = Sauron.name
-                    return winner
+                self.dinosaur_one.attack(self.robot_one)
+                if self.robot_one.health <= 0:
+                    print(f"{self.robot_one.name} has fallen")
+                    winner = self.dinosaur_one.name
+                    break
                     
-                elif Sauron.health <= 0:
-                    print(f"{Sauron.name} has fallen")
-                    winner = Megatron.name
-                    return winner        
+                elif self.dinosaur_one.health <= 0:
+                    print(f"{self.dinosaur_one.name} has fallen")
+                    winner = self.robot_one.name
+                    break        
         else:
             print("Maybe next time!")
     def run_game(self):
--- robot.py
+++ robot.py
@@ -4,10 +4,9 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon
+        self.active_weapon = Weapon("Blaster, Cannon", 40)
     def attack(self, dinosaur):
-        dinosaur.health -= active_weapon.attack_power
-        print(f"{self.name} has blasted {dinosaur.name} with his {active_weapon.name} for {active_weapon.attack_power} damage!")
+        dinosaur.health -= self.active_weapon.attack_power
+        print(f"{self.name} has blasted {dinosaur.name} with his {self.active_weapon.name} for {self.active_weapon.attack_power} damage!")
         print(f"{dinosaur.name} has {dinosaur.health} health remaining!")
         
-active_weapon = Weapon("Blaster Cannon", 40)
