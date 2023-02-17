8ab9647cece7d092a03bd9fab98ea8a046e6e1b2
Gavin Warnock <gbwarnock@gmail.com>
2023-02-17 13:20:10 -0700

    Added and completed the display_winner method

--- battlefield.py
+++ battlefield.py
@@ -9,29 +9,41 @@ class Battlefield:
     def display_welcome(self):
             print(f"Welcome to the {self.name}! Where the futuristic {Megatron.name} will face off against the primal fury of {Sauron.name}!")
             print("")
-            
-                
-    def run_game(self):
-        battlefield.display_welcome()
+    def display_winner(self):
+        if Megatron.health <= 0:
+            print(f"{Sauron.name} is victorious!")
+        elif Sauron.health <= 0:
+            print(f"{Megatron.name} is victorious!")
+    def battle_phase(self):     
         start = input("Are you ready to begin? Yes or No ")
-        if start == "Yes":
+        if start == "Yes" or start == "yes":
             while Megatron.health >= 0 or Sauron.health >= 0:
                 Megatron.attack(Sauron)
                 if Megatron.health <= 0:
-                    print(f"{Megatron.name} has fallen, {Sauron.name} wins!")
-                    break
+                    print(f"{Megatron.name} has fallen")
+                    winner = Sauron.name
+                    return winner
+                    
                 elif Sauron.health <= 0:
-                    print(f"{Sauron.name} has fallen {Megatron.name} wins!")
-                    break
+                    print(f"{Sauron.name} has fallen")
+                    winner = Megatron.name
+                    return winner
+                    
                 Sauron.attack(Megatron)
                 if Megatron.health <= 0:
-                    print(f"{Megatron.name} has fallen, {Sauron.name} wins!")
-                    break
+                    print(f"{Megatron.name} has fallen")
+                    winner = Sauron.name
+                    return winner
+                    
                 elif Sauron.health <= 0:
-                        print(f"{Sauron.name} has fallen {Megatron.name} wins!")
-                        break
+                    print(f"{Sauron.name} has fallen")
+                    winner = Megatron.name
+                    return winner        
         else:
             print("Maybe next time!")
-
-
+    def run_game(self):
+        battlefield.display_welcome()
+        battlefield.battle_phase()
+        battlefield.display_winner()
+       
 battlefield = Battlefield("Battleground of Titans")
