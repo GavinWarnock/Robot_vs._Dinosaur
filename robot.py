8fb04be876b983904f3b0c127e39535da0a14035
Gavin Warnock <gbwarnock@gmail.com>
2023-02-17 14:27:59 -0700

    Bonus task of adding three weapon choices to Robot complete

--- robot.py
+++ robot.py
@@ -4,9 +4,18 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon("Blaster, Cannon", 40)
+        self.active_weapon = [Weapon("Blaster Cannon", 40), Weapon("Beam Sword", 20), Weapon("Fists", 10)] 
     def attack(self, dinosaur):
-        dinosaur.health -= self.active_weapon.attack_power
-        print(f"{self.name} has blasted {dinosaur.name} with his {self.active_weapon.name} for {self.active_weapon.attack_power} damage!")
-        print(f"{dinosaur.name} has {dinosaur.health} health remaining!")
-        
+        weapon_choice = input("Choose your weapon! Blaster Cannon, Beam Sword, or Fists ")
+        if weapon_choice == "Blaster Cannon":
+            dinosaur.health -= self.active_weapon[0].attack_power
+            print(f"{self.name} has struck {dinosaur.name} with his {self.active_weapon[0].name} for {self.active_weapon[0].attack_power} damage!")
+            print(f"{dinosaur.name} has {dinosaur.health} health remaining!")
+        elif weapon_choice == "Beam Sword":
+            dinosaur.health -= self.active_weapon[1].attack_power
+            print(f"{self.name} has struck {dinosaur.name} with his {self.active_weapon[1].name} for {self.active_weapon[1].attack_power} damage!")
+            print(f"{dinosaur.name} has {dinosaur.health} health remaining!") 
+        elif weapon_choice == "Fists":
+            dinosaur.health -= self.active_weapon[2].attack_power
+            print(f"{self.name} has struck {dinosaur.name} with his {self.active_weapon[2].name} for {self.active_weapon[2].attack_power} damage!")
+            print(f"{dinosaur.name} has {dinosaur.health} health remaining!")
